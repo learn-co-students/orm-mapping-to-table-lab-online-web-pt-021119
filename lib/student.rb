d9335b36bb4ec0a7fcd7287eb5e918bed8677285
P0rtk3y <llama.p0rtk3y@gmail.com>
2019-05-03 23:54:25 -0700

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,5 +1,43 @@
 class Student
 
-  attr_reader :id 
+  attr_reader :id
+  attr_accessor :name, :grade
+
+  def initialize(name, grade, id=nil)
+    @name = name
+    @grade = grade
+    @id = id
+  end
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade TEXT
+      );
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = "DROP TABLE students"
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    sql = <<-SQL
+      INSERT INTO students (name, grade) VALUES (?,?)
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students").flatten[0]
+  end
+
+  def self.create(name:, grade:)
+    student = Student.new(name, grade)
+    student.save
+    student
+  end
 
 end
